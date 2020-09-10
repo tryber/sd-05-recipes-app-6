17c2fbc6e026dd02622346e568e9ad5b211d4ff8
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-09-10 16:41:40 -0300

    implementaçao da tela com detalhe das receitas de comida

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/App.css';
+
+function IngredientsList({ recipe, ingredient }) {
+  return (
+    <div key={recipe[`strIngredient${ingredient}`]}>
+      <input type="checkbox" id={recipe[`strIngredient${ingredient}`]} />
+      <label
+        htmlFor={recipe[`strIngredient${ingredient}`]}
+        data-testid={`${ingredient}-ingredient-step`}
+      >
+        {recipe[`strIngredient${ingredient}`]} - {recipe[`strMeasure${ingredient}`]}
+      </label>
+    </div>
+  );
+}
+
+export default IngredientsList;
+
+IngredientsList.propTypes = {
+  recipe: PropTypes.instanceOf(Object).isRequired,
+  ingredient: PropTypes.number.isRequired,
+};
--- src/pages/FoodDetail.js
+++ src/pages/FoodDetail.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { foodIdApi } from '../service/foodApi';
+import IngredientsList from '../components/IngredientsList';
 
-function FoodDetail() {
+function FoodDetail(props) {
+  const ingredients = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    foodIdApi(props.match.params.id).then((response) => {
+      setRecipe(response.meals[0]);
+    });
+    
+  }, []);
+  console.log(recipe);
+
+  if (!recipe.idMeal) return <div>Carregando...</div>;
+
   return (
     <div>
       <Header />
       <div>
-        <h1>FoodDetail</h1>
+        <img data-testid="recipe-photo" src={recipe.strMealThumb} alt={recipe.strMeal} />
+        <h2 data-testid="recipe-title">{recipe.strMeal}</h2>
+        <button data-testid="share-btn">share</button>
+        <button data-testid="favorite-btn">Favorite</button>
+        <h4 data-testid="recipe-category">{recipe.strCategory}</h4>
+        <h3>Ingredients</h3>
+        {ingredients.map((ingredient) => {
+          if(recipe[`strIngredient${ingredient}`] !== "") {
+            return <IngredientsList recipe={recipe} ingredient={ingredient} />
+        }})}
+        <h3>Instructions</h3>
+        <p data-testid="instructions">{recipe.strInstructions}</p>
+        <button data-testid="finish-recipe-btn">Finalizar Receita</button>
       </div>
       <Footer />
     </div>
