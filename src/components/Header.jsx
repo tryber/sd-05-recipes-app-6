b9e6384237d7974063b924c93eee5c589ce7e502
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-09-09 23:53:53 -0300

    codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 import '../styles/header_footer.css';
-import { Link } from 'react-router-dom';
 
 function Header() {
   return (
