2281725eccd7a2225582575ca403dff0050f8917
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-10 18:08:06 -0300

    Tela explorar bebidas

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/header_footer.css';
 
 function ExploreDrinks() {
   return (
     <div>
-      <Header />
-      <div>
-        <h1>ExploreDrinks</h1>
+      <header>
+        <Header />
+      </header>
+      <div className="explore">
+        <Link to="/explorar/bebidas/ingredientes" data-testid="explore-by-ingredient">
+          <button className="btn ing" data-testid="explore-by-ingredient">
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="" data-testid="explore-surprise">
+          <button className="btn surpr" data-testid="explore-surprise">
+            Me Surpreenda!
+          </button>
+        </Link>
       </div>
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
