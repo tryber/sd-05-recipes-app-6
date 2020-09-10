928045223baffde398ab1ed03c59d6dcb87fa5f2
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-09-09 23:52:25 -0300

    adiçao de alguns data-testid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 import '../styles/header_footer.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
-      <img src={profile} alt="Perfil" data-testid="profile-top-btn" />
-      <p>Comida</p>
+      <Link to="/perfil" data-testid="profile-top-btn">
+        <img src={profile} alt="Perfil" data-testid="profile-top-btn" />
+      </Link>
+      <h2 data-testid="page-title">Perfil</h2>
       <img src={search} alt="Procurar" data-testid="search-top-btn" />
     </div>
   );
