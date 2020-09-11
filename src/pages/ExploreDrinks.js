61d5ac623f81123e1639924ff58bc0b02810e924
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-11 17:52:01 -0300

    Requisitos da tela explorar done

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { drinkRandomApi } from '../service/drinkApi';
 // import '../styles/header_footer.css';
 
 function ExploreDrinks() {
+  const [randomId, setRandomId] = useState("")
+
+  useEffect(() => {
+    drinkRandomApi().then((response) => {
+      setRandomId(response['drinks'][0].idDrink)
+    })
+  }, []);
+
   return (
     <div>
       <header>
         <Header title={'Explorar Bebidas'} showSearchIcon={false} />
       </header>
       <div className="explore">
-        <Link to="/explorar/bebidas/ingredientes" data-testid="explore-by-ingredient">
+        <Link to="/explorar/bebidas/ingredientes">
           <button className="btn ing" data-testid="explore-by-ingredient">
             Por Ingredientes
           </button>
         </Link>
-        <Link to="" data-testid="explore-surprise">
+        <Link to={`/bebidas/${randomId}`}>
           <button className="btn surpr" data-testid="explore-surprise">
             Me Surpreenda!
           </button>
