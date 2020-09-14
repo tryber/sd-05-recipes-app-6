a72599c7265a20d93e54a9b07b6d67e530069935
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-09-14 20:40:24 -0300

    pagina de receitas feitas

--- src/pages/MadeReceipes.js
+++ src/pages/MadeReceipes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import MadeReceipesCard from '../components/MadeReceipesCard';
 
 function MadeReceipes() {
   return (
     <div>
       <Header title={'Receitas Feitas'} showSearchIcon={false} />
       <div>
-        <h1>MadeReceipes</h1>
+        <button data-testid="filter-by-all-btn">All</button>
+        <button data-testid="filter-by-food-btn" >Food</button>
+        <button data-testid="filter-by-drink-btn">Drinks</button>
+        <MadeReceipesCard />
       </div>
     </div>
   );
